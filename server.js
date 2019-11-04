0fa47adc81e309436ed960678ce9790a275bebc2
WilliamBerlin76 <william.berlin76@gmail.com>
2019-11-04 16:40:12 -0600

    endpoint to get accounts now working

--- server.js
+++ server.js
@@ -10,4 +10,14 @@ server.get('/', (req, res) => {
     res.send(`<h2>Let's get this party started</h2>`)
 });
 
+server.get('/api/accounts', (req, res) => {
+    db('Accounts')
+        .then(accounts => {
+            res.status(200).json(accounts)
+        })
+        .catch(err => {
+            res.status(500).json({error: 'failed to get accounts'})
+        })
+})
+
 module.exports = server;
